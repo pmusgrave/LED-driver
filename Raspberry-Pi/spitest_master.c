10530c6de3bb8c6f30e15c3d6b007f1d8d4331bb
Paul <ps.musgrave@gmail.com>
2018-12-18 23:16:58 -0500

    SPI communication is working SPI master

--- Raspberry-Pi/spitest_master.c
+++ Raspberry-Pi/spitest_master.c
@@ -15,12 +15,10 @@ int main(void){
     bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
 
     // configure SPI rate to 3.125MHz
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_256);
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_128);
 
     int i = 0;
     while(1){
-      // polls each of four ADC readings sequentially
-
       SPI_read_buffer = bcm2835_spi_transfer(i);
 
       printf("SPI master device sent: %d\tSPI slave device sent: %d\n", i, SPI_read_buffer);
