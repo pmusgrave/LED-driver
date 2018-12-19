717226fc86203ad6ac31b9d0c83362dff01c7402
Paul <ps.musgrave@gmail.com>
2018-12-19 15:48:33 -0500

    test fading all LED colors via SPI

--- Raspberry-Pi/spitest_master.c
+++ Raspberry-Pi/spitest_master.c
@@ -14,23 +14,22 @@ int main(void){
 
     bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
 
-    // configure SPI rate to 3.125MHz
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_128);
+    // configure SPI rate
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32768);
 
     int i = 0;
     while(1){
-      SPI_read_buffer = bcm2835_spi_transfer("R");
-      SPI_read_buffer = bcm2835_spi_transfer(i);
-
-      SPI_read_buffer = bcm2835_spi_transfer("G");
-      SPI_read_buffer = bcm2835_spi_transfer(100);
-
-      SPI_read_buffer = bcm2835_spi_transfer("B");
-      SPI_read_buffer = bcm2835_spi_transfer(100);
       // printf("SPI master device sent: %d\tSPI slave device sent: %d\n", i, SPI_read_buffer);
-      // sleep(1);
-      i++;
+      SPI_read_buffer = bcm2835_spi_transfer(82);
+      SPI_read_buffer = bcm2835_spi_transfer(i%255);
 
+      SPI_read_buffer = bcm2835_spi_transfer(71);
+      SPI_read_buffer = bcm2835_spi_transfer(i%255);
+
+      SPI_read_buffer = bcm2835_spi_transfer(66);
+      SPI_read_buffer = bcm2835_spi_transfer(i%255);      
+
+      i++;
     }
 
     bcm2835_spi_end();
