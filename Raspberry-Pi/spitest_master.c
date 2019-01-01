6be96dea3cf69bd846f7059db2c7e6f09d681747
Paul <ps.musgrave@gmail.com>
2019-01-01 01:52:07 -0500

    SPI master devices sends an incrementing value on all three channels clock rate slowed down as much as possible to avoid missed SPI messages

--- Raspberry-Pi/spitest_master.c
+++ Raspberry-Pi/spitest_master.c
@@ -2,6 +2,10 @@
 #include <unistd.h>
 #include <bcm2835.h>
 
+#define ASCII_R 82
+#define ASCII_G 71
+#define ASCII_B 66
+
 int main(void){
   printf("Initializing SPI configuration...\n");
   bcm2835_init();
@@ -15,21 +19,23 @@ int main(void){
     bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
 
     // configure SPI rate
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32768);
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536);
 
     int i = 0;
     while(1){
       // printf("SPI master device sent: %d\tSPI slave device sent: %d\n", i, SPI_read_buffer);
-      SPI_read_buffer = bcm2835_spi_transfer(82);
-      SPI_read_buffer = bcm2835_spi_transfer(i%255);
-
-      SPI_read_buffer = bcm2835_spi_transfer(71);
-      SPI_read_buffer = bcm2835_spi_transfer(i%255);
-
-      SPI_read_buffer = bcm2835_spi_transfer(66);
-      SPI_read_buffer = bcm2835_spi_transfer(i%255);      
 
       i++;
+      SPI_read_buffer = bcm2835_spi_transfer(ASCII_R);
+      SPI_read_buffer = bcm2835_spi_transfer(i);
+
+      
+      SPI_read_buffer = bcm2835_spi_transfer(ASCII_G);
+      SPI_read_buffer = bcm2835_spi_transfer(i);
+
+      SPI_read_buffer = bcm2835_spi_transfer(ASCII_B);
+      SPI_read_buffer = bcm2835_spi_transfer(i);      
+     
     }
 
     bcm2835_spi_end();
